5334c284317a209b4fef0fd940bf601e9cb8503f
nicolas-kruger <nicolas.nkruger@gmail.com>
2021-06-17 11:50:41 -0300

    by pass cors carralho

--- complete/src/main/java/com/example/messagingstompwebsocket/WebSocketConfig.java
+++ complete/src/main/java/com/example/messagingstompwebsocket/WebSocketConfig.java
@@ -1,14 +1,25 @@
 package com.example.messagingstompwebsocket;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.MessageHandler;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+
+
 
 	@Override
 	public void configureMessageBroker(MessageBrokerRegistry config) {
@@ -18,7 +29,10 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
-		registry.addEndpoint("/gs-guide-websocket").withSockJS();
+		registry.addEndpoint("/gs-guide-websocket")
+				.setAllowedOriginPatterns("*")
+				.withSockJS();
+
 	}
 
 }
